b4fdd17bb4caa2f707c1e1b18996b9b6690dae2a
AntonOshurek <oshurekweb@gmail.com>
2023-08-17 17:24:27 +0200

    add lib for open image to fullscrean

--- src/scripts/components/projects-gallery.js
+++ src/scripts/components/projects-gallery.js
@@ -1,10 +1,10 @@
 import Masonry from "masonry-layout";
 
 const projectsGallery = () => {
-  const grid = document.querySelector('.grid');
+  const grid = document.querySelector('.projects__list');
   // eslint-disable-next-line no-unused-vars
   const masonry = new Masonry(grid, {
-    itemSelector: '.grid-item',
+    itemSelector: '.projects__item',
     percentPosition: true,
 		gutter: 43,
 		originCenter: true,
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
 import { mobileMenu } from "./components/mobile-menu.js";
 import { projectsGallery } from "./components/projects-gallery.js";
+import { fullscreenImage } from "./components/fullscreen-image.js";
 
 window.addEventListener('DOMContentLoaded', () => {
 	mobileMenu();
 	projectsGallery();
+	fullscreenImage();
 });
